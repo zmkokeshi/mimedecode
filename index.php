885b1816188987b5d565ad40bcd3e13b8a2a8553
zmkokeshi <zmkzmzm2512@gmail.com>
2016-03-22 17:46:06 +0900

    add:mimedecodeの結果をHTMLの上に表示

--- index.php
+++ index.php
@@ -5,21 +5,38 @@
     <title>test</title>
 </head>
 <body>
-えべろーぶ：
-<form action = "index.php" method = "get">
-    <input type="text" name="decode">
+エンベロープ：
+<form action = "index.php" method = "post">
+    <textarea name="decode" cols=60 rows=40>
+  </textarea>
     <input type="submit" value="でこーど">
-<?php
-echo 'test';
-
-?>
 </form>
 </body>
 </html>
 
 <?php
-$comment = $_GET['decode'];
-echo $comment;
-echo "test";
-var_dump($comment);
+$comment = $_POST['decode'];
+
+//メールをMimeデコードをして分解
+//MimeDecodeに渡す際にDcodeの項目等をtureにしておく
+// /usr/share/pear/Mail/mimeDecode.php　詳細はこのファイルで
+
+require_once 'Mail/mimeDecode.php';
+$params['include_bodies'] = true;
+$params['decode_bodies']  = true;
+$params['decode_headers'] = true;
+$params['input'] = $comment;
+$params['crlf'] = "\r\n";
+
+
+//デコード
+$mail_data = Mail_mimeDecode::decode($params);
+
+
+//mime_Decodoで帰ってきたオブジェクトにアクセスし値を取得。
+
+$FromAddress = $mail_data->headers['from'];
+var_dump($FromAddress);
+var_dump($mail_data);
+
 ?>
