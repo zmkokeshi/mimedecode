6cf980642e863d8b3937b89483fcd1402d445fc5
zmkokeshi <zmkokeshi@gmail.com>
2016-03-22 15:07:09 +0900

    modify:拡張子の変更

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title>test</title>
+</head>
+<body>
+えべろーぶ：<input type="text" name="namae" size="40" maxlength="20">
+<input type="submit" value="でこーど">
+</body>
+</html>
