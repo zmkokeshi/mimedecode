4e19636af4936f19c0ce715430be94d35ec06cd0
zmkokeshi <zmkokeshi@gmail.com>
2016-03-23 17:26:11 +0900

    modify:概ね表示されるようになった

--- index.php
+++ index.php
@@ -17,21 +17,35 @@
 
 <?php
 
-function keyloop($input){
 
-    //var_dump($input);
+function keyloop($input)
+
+{
     foreach($input as $key1=>$key2) {
+        if (is_array($key2)||is_object($key2) ){
 
-        if (is_array($key2)) {
-            //var_dump($key2);
-            keyloop($key2);
-            echo "arrayだったから呼び出し";
-            echo "</br>";
-
-        } else {
+            echo "<h1>";
             echo $key1;
+            echo "</h1>";
             echo "</br>";
-            echo $key2;
+            keyloop($key2);
+
+
+        }elseif(is_array($key1)||is_object($key1)){
+            echo "<h1>";
+            echo $key1;
+            echo "</h1>";
+            echo "</br>";
+            keyloop($key1);
+
+        }
+        else {
+            echo "<h1>";
+            echo $key1;
+            echo "</h1>";
+            echo "</br>";
+            #echo $key2;
+            var_dump($key2);
             echo "</br>";
             echo "--------------------------------------------------------";
             echo "</br>";
@@ -60,33 +74,11 @@ $mail_data = Mail_mimeDecode::decode($params);
 
 //mime_Decodoで帰ってきたオブジェクトにアクセスし値を取得。
 
-$FromAddress = $mail_data->headers['from'];
-
 echo "</br>";
 echo "</br>";
-
 
 
 keyloop($mail_data);
 
-
-if(is_array($mail_data->headers)){
-
-    echo" 配列だったみたい";
-#var_dump($mail_data->headers);
-    foreach($mail_data->headers as $Headre)
-    {
-        // echo key($mail_data);
-        //var_dump($Headre);
-        // echo ($Headre);
-        echo "</br>";
-
-    }
-}
-
-else
-{
-    echo "処理ができてないお";
-}
 
 ?>
