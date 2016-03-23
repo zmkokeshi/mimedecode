2ad97609051f4f5ae1db1537278233426316cd77
zmkokeshi <zmkokeshi@gmail.com>
2016-03-23 15:05:21 +0900

    modify:連想配列からキーを取得（途中

--- index.php
+++ index.php
@@ -8,13 +8,39 @@
 エンベロープ：
 <form action = "index.php" method = "post">
     <textarea name="decode" cols=60 rows=40>
-  </textarea>
+
+    </textarea>
     <input type="submit" value="でこーど">
 </form>
 </body>
 </html>
 
 <?php
+
+function keyloop($input){
+
+    //var_dump($input);
+    foreach($input as $key1=>$key2) {
+
+        if (is_array($key2)) {
+            //var_dump($key2);
+            keyloop($key2);
+            echo "arrayだったから呼び出し";
+            echo "</br>";
+
+        } else {
+            echo $key1;
+            echo "</br>";
+            echo $key2;
+            echo "</br>";
+            echo "--------------------------------------------------------";
+            echo "</br>";
+
+        }
+    }
+
+}
+
 $comment = $_POST['decode'];
 
 //メールをMimeデコードをして分解
@@ -32,11 +58,35 @@ $params['crlf'] = "\r\n";
 //デコード
 $mail_data = Mail_mimeDecode::decode($params);
 
-
 //mime_Decodoで帰ってきたオブジェクトにアクセスし値を取得。
 
 $FromAddress = $mail_data->headers['from'];
-var_dump($FromAddress);
-var_dump($mail_data);
+
+echo "</br>";
+echo "</br>";
+
+
+
+keyloop($mail_data);
+
+
+if(is_array($mail_data->headers)){
+
+    echo" 配列だったみたい";
+#var_dump($mail_data->headers);
+    foreach($mail_data->headers as $Headre)
+    {
+        // echo key($mail_data);
+        //var_dump($Headre);
+        // echo ($Headre);
+        echo "</br>";
+
+    }
+}
+
+else
+{
+    echo "処理ができてないお";
+}
 
 ?>
