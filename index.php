aa3b16c1e30d523922af5b3542db635011019d09
zmkokeshi <zmkokeshi@gmail.com>
2016-03-30 13:46:08 +0900

    add:bootstrap導入

--- index.php
+++ index.php
@@ -2,18 +2,19 @@
 <html>
 <head lang="en">
     <meta charset="UTF-8">
-    <title>test</title>
+    <title> エンベロープを快適に見よう</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 </head>
 <body>
-エンベロープ：
+
+<section class="row col-xs-8 col-xs-offset-2">
+<h1 class='text-center'>エンベロープ</h1>
 <form action = "index.php" method = "post">
-    <textarea name="decode" cols=60 rows=40>
+    <textarea name="decode" cols=200 rows=10>
 
     </textarea>
-    <input type="submit" value="でこーど">
-</form>
-</body>
-</html>
+    <button type="submit" class="btn btn-info center-block btn-lg">でこーど</button>
 
 <?php
 
@@ -22,34 +23,25 @@ function keyloop($input)
 
 {
 
+
     foreach($input as $key1=>$key2) {
         if (is_array($key2)||is_object($key2) ){
 
-            echo "<h1>";
+            echo "<h1 class='text-center'>";
             echo $key1;
             echo "</h1>";
             echo "</br>";
             keyloop($key2);
-
-
-        }elseif(is_array($key1)||is_object($key1)){
-            echo "<h1>";
-            echo $key1;
-            echo "</h1>";
-            echo "</br>";
-            keyloop($key1);
-            echo "key1";
-
         }
         else {
-            echo "<h2>";
+            echo "<h2 class='text-center'>";
             echo $key1;
             echo "</h2>";
             echo "</br>";
-            #echo $key2;
-            var_dump($key2);
-            echo "</br>";
-            echo "------------------key2--------------------------------------";
+            echo "<div class='text-center'>";
+            echo $key2;
+            echo "</div>";
+//            var_dump($key2);
             echo "</br>";
 
         }
@@ -81,17 +73,19 @@ echo "</br>";
 
 $From=$mail_data->headers['from'];
 #var_dump($From);
+echo('<pre>');
 var_dump($mail_data);
+echo('</pre>');
 echo '</br>';
 echo '</br>';
 echo '</br>';
 echo '==============================[元のオブジェクト]=============================';
 
-
-
 keyloop($mail_data);
 
 
-
-
 ?>
+</form>
+</section>
+</body>
+</html>
